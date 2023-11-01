9747c8f418e4d1319e4f1a27058795de63325c33
GCodeON <gcodeondev@gmail.com>
2023-11-01 09:42:18 -0700

    api error handler

--- pages/api/add-customer.js
+++ pages/api/add-customer.js
@@ -2,21 +2,30 @@
 import Prisma from '../../prisma/prisma';
 
 export default async function handler(req, res) {
-  const formData = req.body;
+  if (req.method === 'POST') {
+    const formData = req.body;
 
-  const result = await prisma.customer.create({
-    data: formData
-  })
+    try {
+
+      const result = await prisma.customer.create({
+        data: formData
+      })
+      
+      console.log('display data', result);
   
-  console.log('display data', result);
+      if(result) {
+  
+        res.status(200).send({ 
+          customer: result,
+          message: 'Form Successfully Submitted!',
+          submitted: true
+        });
+      }
+    } catch (err) {
+      res.status(500).json({ error: 'failed to load data' })
+    }
 
-  if(result) {
 
-    res.send({ 
-      customer: result,
-      message: 'Form Successfully Submitted!',
-      submitted: true
-    });
   }
-
+  
 };
