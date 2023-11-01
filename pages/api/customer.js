6fa52ba91edcec6f14266dc5bf2387e181f8b426
GCodeON <gcodeondev@gmail.com>
2023-11-01 13:46:50 -0700

    prisma fix

--- pages/api/customer.js
+++ pages/api/customer.js
@@ -1,5 +1,7 @@
 
-import prisma from '../../prisma/prisma';
+// import prisma from '../../prisma/prisma';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
